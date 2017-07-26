bf4f6b0d933af922549eaab488aed825ff9df086
BoberCoder <eughene87@ukr.net>
2017-07-26 17:48:42 +0300

    fix examples for two methods

--- examples/exampleCompetitorsMethod.php
+++ examples/exampleCompetitorsMethod.php
@@ -13,6 +13,7 @@ $config = [
     'token' => '19666fc1ae1724da1d5ea2f3a99d5f5a',
 ];
 
+$domain = 'example.com';
 $keywords = 'keywords';
 
 // init client with your serpstat api token
@@ -20,15 +21,22 @@ $apiClient = new \Serpstat\Sdk\Core\ApiGuzzleHttpClient($config['token']);
 
 // create instance of any api method class
 // list of methods classes in folder src\Methods
-$apiMethod = new \Serpstat\Sdk\Methods\AdKeywordsMethod(
+$apiMethodDomain = new \Serpstat\Sdk\Methods\CompetitorsMethod(
+    $domain,
+    \Serpstat\Sdk\Interfaces\IApiClient::SE_GOOGLE_RU
+);
+
+$apiMethodKeywords = new \Serpstat\Sdk\Methods\CompetitorsMethod(
     $keywords,
     \Serpstat\Sdk\Interfaces\IApiClient::SE_GOOGLE_RU
 );
 
 try {
     // try call api method
-    $response = $apiClient->call($apiMethod);
+    $response1 = $apiClient->call($apiMethodDomain);
+    $response2 = $apiClient->call($apiMethodKeywords);
 } catch (\Exception $e) {
     // catch api error
-    $response = $e->getMessage();
+    $response1 = $e->getMessage();
+    $response2 = $e->getMessage();
 }
